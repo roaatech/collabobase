b375322321c489eb47fb6ae42a07ddd7a97d9b43
Muhannad <Muhannad@mhh1422>
2014-03-28 08:48:47 +0400

    Optimizing system upgrader.

--- app/controllers/upgrade.php
+++ app/controllers/upgrade.php
@@ -8,19 +8,40 @@ class Upgrade extends MY_Controller {
     }
 
     public function index() {
-        $version = $this->config->item('app_version');
-        if (!$version) {
-            $version = 0;
+        $curVer = @file_get_contents(APPPATH . "config" . DIRECTORY_SEPARATOR . "version.txt");
+        if (!$curVer) {
+            $curVer = 1;
         }
+        $curVer+=0;
+        $lasVer = $this->config->item('app_version');
 
         $this->output->set_content_type("text/plain; charset=UTF-8");
 
-        switch ($version) {
-            case 0:
+        echo "Collabobase System Upgrader\n";
+        echo "===========================\n";
+        echo "\n";
+        echo "Current version: {$curVer}\n";
+        echo "Last version: {$lasVer}\n";
+        echo "\n";
+
+        switch ($curVer) {
+            case 1:
                 echo "Upgrading to v1.1:\n";
+                echo "------------------\n";
                 $this->exec("ALTER TABLE `chat_participant` ADD `last_check_chat_message_id` INT NULL , ADD INDEX (`last_check_chat_message_id`) ;", "Altering chat_participant table");
                 $this->exec("update chat_participant p set last_check_chat_message_id = (select max(id) from chat_message where time<=p.last_check and chat_id = p.chat_id)", "Updaing chat_participant table data");
+                echo "\n";
+
+
+                //everything is before this
+                echo "Setting config to v{$lasVer}:\n";
+                file_put_contents(APPPATH . "config" . DIRECTORY_SEPARATOR . "version.txt", $lasVer);
+                echo "Done\n";
+                break;
+            default:
+                echo "Nothing to upgrade to!\n";
         }
+        echo "\n";
     }
 
     protected function exec($stmt, $title) {
